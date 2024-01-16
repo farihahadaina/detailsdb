39057bd400c2bb1d7979ee95c1f7be60bf0c40c7
nadirah <nadirahros.liza@live.iium.edu.my>
2024-01-16 22:29:53 +0800

    Update homescreen.dart

--- detailsdb3/lib/homescreen.dart
+++ detailsdb3/lib/homescreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:detailsdb/inputscreen.dart';
+import 'package:firebase_database/firebase_database.dart';
+
 
 class MyHomeScreen extends StatefulWidget {
   const MyHomeScreen({super.key});
@@ -7,7 +10,7 @@ class MyHomeScreen extends StatefulWidget {
   @override
   State<MyHomeScreen> createState() => _MyHomeScreenState();
 }
-
+final databaseReference = FirebaseDatabase.instance.ref("Users");
 class _MyHomeScreenState extends State<MyHomeScreen> {
   @override
   Widget build(BuildContext context) {
@@ -28,6 +31,30 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
           fontWeight: FontWeight.bold
         ),),
       ],),),
-      );
+ body: Column(
+        children: [
+          Expanded(
+              child: FirebaseAnimatedList(
+                  query: databaseReference,
+                  itemBuilder: (context, snapshot, index, animation) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      margin: const EdgeInsets.all(10),
+                      child: ListTile(
+                        title: Text(
+                          snapshot.child("name").value.toString(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                            ),
+                      ),
+                    );
+                  })),
+        ],
+      ),
+    );
   }
 }
